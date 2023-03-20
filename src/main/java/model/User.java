5e6f4ff0f3c60d1c84fc6264616ca1dd6db4bbf1
Василиса <vasilisaraist@yandex.ru>
2023-03-20 16:43:49 +0300

    - созданы классы User и Film;

---
+++ src/main/java/model/User.java
@@ -0,0 +1,17 @@
+package model;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class User {
+    private final int id;
+    private final String email;
+    private final String login;
+    private final String name;
+    private final LocalDate birthday;
+
+
+
+}
