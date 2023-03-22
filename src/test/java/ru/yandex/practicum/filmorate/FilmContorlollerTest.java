fe117e5ebc7af4c228fb0c861a4652a10a0498a1
Василиса <vasilisaraist@yandex.ru>
2023-03-22 15:16:51 +0300

    - тесты разбиты на два класса;

--- src/test/java/ru/yandex/practicum/filmorate/FilmContorlollerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmContorlollerTest.java
@@ -1,84 +1,22 @@
 package ru.yandex.practicum.filmorate;
 
 import controller.FilmController;
-import controller.UserController;
 import exception.ValidateException;
 import model.Film;
-import model.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.Duration;
 import java.time.LocalDate;
 
-@SpringBootTest
-class FilmorateApplicationTests {
-
-    @Test
-    void validateEmailTest() {
-        UserController userController = new UserController();
-
-        User user = getDefaultUser();
-        user.setEmail("NOT_A_EMAIL");
-
-        try {
-            userController.validateNewUser(user);
-        } catch (ValidateException e) {
-            Assertions.assertEquals("Неправильный email пользователя", e.getMessage());
-        }
-
-        user.setEmail(null);
-
-        try {
-            userController.validateNewUser(user);
-        } catch (ValidateException e) {
-            Assertions.assertEquals("Неправильный email пользователя", e.getMessage());
-        }
-    }
-
-    @Test
-    void validateNameTest() {
-        UserController userController = new UserController();
-
-        User user = getDefaultUser();
-        user.setName(null);
-        user.setLogin(null);
-
-        try {
-            userController.validateNewUser(user);
-        } catch (ValidateException e) {
-            Assertions.assertEquals("Имя пользователя и логин являются пустыми", e.getMessage());
-        }
-
-    }
-
-    @Test
-    void validateLoginTest() {
-        UserController userController = new UserController();
-
-        User user = getDefaultUser();
-        user.setLogin(null);
-
-        try {
-            userController.validateNewUser(user);
-        } catch (ValidateException e) {
-            Assertions.assertEquals("Неправильный логин пользователя!", e.getMessage());
-        }
-
-    }
-
-    @Test
-    void validateBirthdayTest() {
-        UserController userController = new UserController();
-
-        User user = getDefaultUser();
-        user.setBirthday(LocalDate.now().plusMonths(1));
-        try {
-            userController.validateNewUser(user);
-        } catch (ValidateException e) {
-            Assertions.assertEquals("Неправильная дата рождения пользователя", e.getMessage());
-        }
+public class FilmContorlollerTest {
+    private Film getDefaultFilm() {
+        Film film = new Film();
+        film.setName("Звёздные войны: Эпизод 1 - Скрытая угроза");
+        film.setDescription("Мирная и процветающая планета Набу. Торговая федерация, не желая платить налоги, вступает в прямой конфликт с королевой Амидалой.");
+        film.setDuration(Duration.ofMinutes(136));
+        film.setReleaseDate(LocalDate.of(1999, 05, 04));
+        return film;
     }
 
     @Test
@@ -139,22 +77,5 @@ class FilmorateApplicationTests {
             Assertions.assertEquals("Дата релиза не может быть раньше 28 декабря 1895 года", e.getMessage());
         }
     }
-
-    private User getDefaultUser() {
-        User user = new User();
-        user.setName("NAME");
-        user.setLogin("LOGIN");
-        user.setEmail("email@example.ru");
-        user.setBirthday(LocalDate.of(2002, 04, 29));
-        return user;
-    }
-
-    private Film getDefaultFilm() {
-        Film film = new Film();
-        film.setName("Звёздные войны: Эпизод 1 - Скрытая угроза");
-        film.setDescription("Мирная и процветающая планета Набу. Торговая федерация, не желая платить налоги, вступает в прямой конфликт с королевой Амидалой.");
-        film.setDuration(Duration.ofMinutes(136));
-        film.setReleaseDate(LocalDate.of(1999, 05, 04));
-        return film;
-    }
 }
+
