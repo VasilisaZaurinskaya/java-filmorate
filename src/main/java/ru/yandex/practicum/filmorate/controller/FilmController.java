9992e8a9382c54cfa9d9718cb97b86853d4dccbb
Василиса <vasilisaraist@yandex.ru>
2023-03-27 13:36:01 +0300

    - исправлены все замечания;

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,14 +1,15 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.ValidateException;
-import lombok.extern.slf4j.Slf4j;
 import ru.yandex.practicum.filmorate.model.Film;
-
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
+
 
 @RestController
 @RequestMapping("/films")
@@ -16,13 +17,20 @@ import java.util.HashMap;
 public class FilmController {
     protected HashMap<Long, Film> filmStorage = new HashMap<>();
 
+
     public FilmController() {
     }
 
     @PostMapping
     public @ResponseBody Film create(@RequestBody Film film) throws ValidateException {
 
-        validateNewFilm(film);
+        try {
+            validateNewFilm(film);
+        } catch (ValidateException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+
         film.setId(generateNewId());
         if (film.getName() == null) {
             throw new ValidateException("Название фильма не может быть пустым");
@@ -33,7 +41,13 @@ public class FilmController {
 
     @PutMapping
     public @ResponseBody Film update(@RequestBody Film film) throws ValidateException {
-        validateNewFilm(film);
+        try {
+            validateNewFilm(film);
+        } catch (ValidateException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+
         if (film.getId() == null || !filmStorage.containsKey(film.getId())) {
             throw new ValidateException("id  не может быть null");
         } else {
@@ -44,9 +58,11 @@ public class FilmController {
     }
 
     public void validateNewFilm(Film film) throws ValidateException {
+
         if (film.getName() == null || film.getName().isEmpty()) {
             throw new ValidateException("Название фильма не может быть пустым");
         }
+
         if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
             throw new ValidateException("Дата релиза не может быть раньше 28 декабря 1895 года");
         }
