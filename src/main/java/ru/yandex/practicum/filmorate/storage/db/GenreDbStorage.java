3d407002bf827f1054fc2b76977ad44e3974e8df
Василиса <vasilisaraist@yandex.ru>
2023-05-12 23:12:02 +0300

    - убран ещё один импорт;

--- src/main/java/ru/yandex/practicum/filmorate/storage/db/GenreDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/db/GenreDbStorage.java
@@ -7,7 +7,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.Genre;
-import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.GenreStorage;
 
 import java.util.ArrayList;
