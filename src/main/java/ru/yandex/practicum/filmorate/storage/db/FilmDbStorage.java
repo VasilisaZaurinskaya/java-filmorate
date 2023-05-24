7968ac0b149799707cf8bdc1bfe671c9e98a0f52
Nuby <nuby.4register@gmail.com>
2023-05-24 19:49:57 +0300

    fix: строка запроса вынесена константой в интерфейс FilmStorage

--- src/main/java/ru/yandex/practicum/filmorate/storage/db/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/db/FilmDbStorage.java
@@ -270,25 +270,7 @@ public class FilmDbStorage implements FilmStorage {
 
     @Override
     public List<Film> getRecommendations(Integer userId) {
-        String recommendedFilms = "SELECT F.film_id, F.name AS film_name, " +
-                "F.description AS film_description, F.release_date, F.duration, F.mpa_rating_id, M.name AS rating_name, " +
-                "GROUP_CONCAT(G.name SEPARATOR ', ') AS genres " +
-                "FROM likes AS L " +
-                "JOIN films AS F ON F.film_id = L.film_id " +
-                "JOIN mpa_rating AS M ON F.mpa_rating_id = M.mpa_rating_id " +
-                "LEFT JOIN genre_film AS GF ON F.film_id = GF.film_id " +
-                "LEFT JOIN genres AS G ON GF.genre_id = G.genre_id " +
-                "WHERE L.film_id NOT IN (SELECT film_id FROM likes WHERE user_id = ?) " +
-                "  AND L.user_id IN (SELECT user_id " +
-                "                    FROM likes " +
-                "                    WHERE film_id IN (SELECT film_id FROM likes WHERE user_id = ?) " +
-                "                      AND user_id != ? " +
-                "                    GROUP BY user_id " +
-                "                    ORDER BY COUNT(film_id) DESC " +
-                "                    LIMIT 1) " +
-                "GROUP BY F.film_id;";
-
-        return jdbcTemplate.query(recommendedFilms, new Object[]{userId, userId, userId}, (rs, rowNum) -> {
+        return jdbcTemplate.query(RECOMMENDED_FILMS, new Object[]{userId, userId, userId}, (rs, rowNum) -> {
             Film film = new Film();
             film.setId(rs.getLong("film_id"));
             film.setName(rs.getString("film_name"));
