941cfddb91d94f33e18e00231378da91d21498bf
ArtemZaretskii <elimuh@ya.ru>
2023-05-29 11:00:54 +0300

    fix: import

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,16 +1,11 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.ValidateException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.service.FilmService;
-
-
-import javax.websocket.server.PathParam;
-
 
 import java.util.List;
 import java.util.Optional;
